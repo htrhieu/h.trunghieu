0554d356a02b546c106ee3a3fcd066731e963e67
htrhieu <125681373+htrhieu@users.noreply.github.com>
2023-03-06 11:29:07 +0700

    Add files via upload

---
+++ Bai2.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+ 
+public class Bai2 {
+    public static void main(String[] args) {
+        int n;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap vao n:");
+        n = scanner.nextInt();
+        if (n % 2 == 0) {
+            System.out.println(n + " La so chan !");
+        } else {
+            System.out.println(n + " La so le !");
+        }
+    }
+}
